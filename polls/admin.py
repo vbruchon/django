663835b14d38112ec561d5153e2e90a39ccbdfe3
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-10-25 14:43:26 +0200

    feat: Create an applications for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
